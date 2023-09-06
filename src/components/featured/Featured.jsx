105b3d5fa4d2f56545e433bea7dae5c14d908244
Mohamed Habib Frigui <mohamedhabibfrigui9@gmail.com>
2023-09-06 10:10:03 +0100

    Add featured and category list

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,7 +1,31 @@
+import Image from "next/image";
 import styles from "./featured.module.css";
 
 const Featured = () => {
-  return <div className={styles.container}>Featured</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, habib here!</b> Discover my stories and creative ideas.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas totam
+            dolores deleniti magnam ullam inventore at porro ratione quo. Velit
+            aliquam debitis reprehenderit corporis vel repellendus expedita ea
+            repudiandae impedit.
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
