ca8196bd48ad4d1f7d0c2f84a93ffec77dfd0ffb
Mohamed Habib Frigui <mohamedhabibfrigui9@gmail.com>
2023-09-06 10:50:14 +0100

    Add home page menu

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,7 +1,25 @@
+import Link from "next/link";
 import styles from "./menu.module.css";
+import Image from "next/image";
+import MenuPosts from "../menuPosts/MenuPosts";
+import MenuCategories from "../menuCategories/MenuCategories";
 
 const Menu = () => {
-  return <div className={styles.container}>Menu</div>;
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.subtitle}>What's hot</h2>
+      <h1 className={styles.title}>Most Popular</h1>
+      <MenuPosts />
+
+      <h2 className={styles.subtitle}>Discover by topics</h2>
+      <h1 className={styles.title}>Categories</h1>
+      <MenuCategories />
+
+      <h2 className={styles.subtitle}>Chosen by the editor</h2>
+      <h1 className={styles.title}>Editors Pick</h1>
+      <MenuPosts withImage />
+    </div>
+  );
 };
 
 export default Menu;
