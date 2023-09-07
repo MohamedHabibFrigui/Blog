1370b902c1ec98393afd41fb9fb2c9949ec5530a
Mohamed Habib Frigui <mohamedhabibfrigui9@gmail.com>
2023-09-07 11:10:05 +0100

    Fetch categories and posts

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,19 +2,21 @@ import Image from "next/image";
 import styles from "./card.module.css";
 import Link from "next/link";
 
-const Card = () => {
+const Card = ({ key, item }) => {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} key={key}>
       <div className={styles.imageContainer}>
-        <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+        {item.img && (
+          <Image src={item.img} alt="" fill className={styles.image} />
+        )}
       </div>
       <div className={styles.textContainer}>
         <div className={styles.detail}>
           <span className={styles.date}>11.02.2023 - </span>
-          <span className={styles.category}>CULTURE</span>
+          <span className={styles.category}>{item.catSlug}</span>
         </div>
         <Link href="/">
-          <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+          <h1>{item.title}</h1>
         </Link>
         <p className={styles.desc}>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et dicta
