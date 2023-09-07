0f1b921b8d710fd0dae704888fb4e065b1bab94b
Mohamed Habib Frigui <mohamedhabibfrigui9@gmail.com>
2023-09-07 09:34:28 +0100

    Add authentication with google

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/footer/Footer";
 import { ThemecontextProvider } from "@/context/ThemeContext";
 import ThemeProvider from "@/providers/ThemeProvider";
+import AuthProvider from "@/providers/AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,17 +17,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemecontextProvider>
-          <ThemeProvider>
-            <div className="container">
-              <div className="wrapper">
-                <Navbar />
-                {children}
-                <Footer />
+        <AuthProvider>
+          <ThemecontextProvider>
+            <ThemeProvider>
+              <div className="container">
+                <div className="wrapper">
+                  <Navbar />
+                  {children}
+                  <Footer />
+                </div>
               </div>
-            </div>
-          </ThemeProvider>
-        </ThemecontextProvider>
+            </ThemeProvider>
+          </ThemecontextProvider>
+        </AuthProvider>
       </body>
     </html>
   );
