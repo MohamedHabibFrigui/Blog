4d5a55adb22f75ca5dbb12748d198906b0c5bb00
Mohamed Habib Frigui <mohamedhabibfrigui9@gmail.com>
2023-09-07 15:33:44 +0100

    Fetch a single post with the user informations

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -1,7 +1,7 @@
 import prisma from "@/utils/connect";
 import { NextResponse } from "next/server";
 
-export const GET = async () => {
+export const GET = async (req) => {
   const { searchParams } = new URL(req.url);
   const page = searchParams.get("page");
   const POST_PER_PAGE = 2;
